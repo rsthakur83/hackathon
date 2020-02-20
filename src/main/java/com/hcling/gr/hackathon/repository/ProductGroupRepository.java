4d5e8d17a430d3b9fe4506527929b391b34629a3
Shubham Rastogi <ershubham88@gmail.com>
2020-02-20 15:41:25 +0100

    commit

--- src/main/java/com/hcling/gr/hackathon/repository/ProductGroupRepository.java
+++ src/main/java/com/hcling/gr/hackathon/repository/ProductGroupRepository.java
@@ -2,9 +2,11 @@ package com.hcling.gr.hackathon.repository;
 
 import com.hcling.gr.hackathon.model.ProductGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * The interface Product group repository.
  */
+@Repository
 public interface ProductGroupRepository extends JpaRepository<ProductGroup, Long> {
 }
--- src/main/java/com/hcling/gr/hackathon/security/MyUserDetailsService.java
+++ src/main/java/com/hcling/gr/hackathon/security/MyUserDetailsService.java
@@ -1,28 +1,28 @@
-package com.hcling.gr.hackathon.security;
-
-import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import com.hcling.gr.hackathon.model.MyUserDetails;
-import com.hcling.gr.hackathon.model.User;
-
-@Service
-	public class MyUserDetailsService implements UserDetailsService {
-
-	    @Autowired
-	    UserRepository userRepository;
-
-	    @Override
-	    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-	        Optional<User> user = userRepository.findByLoginName(userName);
-
-	        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
-
-	        return user.map(MyUserDetails::new).get();
-	    }
-
-}
+/*
+ * package com.hcling.gr.hackathon.security;
+ * 
+ * import java.util.Optional; import
+ * org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.security.core.userdetails.UserDetails; import
+ * org.springframework.security.core.userdetails.UserDetailsService; import
+ * org.springframework.security.core.userdetails.UsernameNotFoundException;
+ * import org.springframework.stereotype.Service;
+ * 
+ * import com.hcling.gr.hackathon.model.MyUserDetails; import
+ * com.hcling.gr.hackathon.model.User;
+ * 
+ * @Service public class MyUserDetailsService implements UserDetailsService {
+ * 
+ * @Autowired UserRepository userRepository;
+ * 
+ * @Override public UserDetails loadUserByUsername(String userName) throws
+ * UsernameNotFoundException { Optional<User> user =
+ * userRepository.findByLoginName(userName);
+ * 
+ * user.orElseThrow(() -> new UsernameNotFoundException("Not found: " +
+ * userName));
+ * 
+ * return user.map(MyUserDetails::new).get(); }
+ * 
+ * }
+ */
