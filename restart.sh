8f064881e6cf41fbf9594c1509a395c22932cf2d
root <root@ip-172-31-45-9.eu-west-3.compute.internal>
2020-02-20 17:08:26 +0000

    restart.sh added

---
+++ restart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ps -ef|grep hackathon-0.0.1-SNAPSHOT.jar|head -1|awk '{print $2}'|xargs kill -9
+
+
+if [ $? -eq "0" ]
+  then
+
+echo 'Restart the backend'
+
+cd /backend/; java -jar hackathon-0.0.1-SNAPSHOT.jar &
+
+else
+
+echo 'Not running start now'
+cd /backend/; java -jar hackathon-0.0.1-SNAPSHOT.jar &
+
+fi
